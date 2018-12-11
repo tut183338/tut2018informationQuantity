094e80fa5a4f2f1edeb213a741faf44e2d2f3249
tut183338 <a183338@edu.tut.ac.jp>
2018-12-11 18:30:51 +0900

    20181211Hello.java変更

--- s4/B183338/Hello.java
+++ s4/B183338/Hello.java
@@ -1,8 +1,8 @@
 package s4.B183338;
 import java.lang.*;
 
-public class Hello {
-    public static void main(String[] args) {
+public class Hello{
+    public static void main(String[] args){
 	System.out.println("hello..(buggy)");
     }
 }
